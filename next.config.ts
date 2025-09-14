849ffa4f7e89fa1525c8c36f76e6e89f22da66d9
seongjongju <sjj990480@gmail.com>
2025-09-14 16:14:43 +0900

    회원가입 완료, 이메일인증, 인증번호 발송 시 딜레이 되는 부분 모달창으로 ux표시, 로그인 완료 페이지, 로그인 로직 작업 예정

--- next.config.ts
+++ next.config.ts
@@ -8,20 +8,18 @@ const withPWA = withPWAInit({
   dest: "public",
   register: true,
   skipWaiting: true,
-  disable: isDev, // dev에서는 PWA 비활성화
+  disable: isDev, 
   buildExcludes: [
-    /app-build-manifest\.json$/, // _next 관련 동적 파일 precache 제외
+    /app-build-manifest\.json$/, 
     /react-loadable-manifest\.json$/,
     /build-manifest\.json$/,
     /middleware-manifest\.json$/
   ],
-  runtimeCaching, // 기본 runtime caching 사용
+  runtimeCaching,
 });
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  // 필요하면 추가 설정
-  // basePath, rewrites 등
 };
 
 export default withPWA(nextConfig);
