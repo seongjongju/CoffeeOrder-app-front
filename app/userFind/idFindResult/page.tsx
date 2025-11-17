f76284d5dcad73145db4fbae6731ed9697252267
seongjongju <sjj990480@gmail.com>
2025-11-17 22:03:24 +0900

    비밀 번호 찾기 해야함

--- app/userFind/idFindResult/page.tsx
+++ app/userFind/idFindResult/page.tsx
@@ -1,16 +1,11 @@
 'use client';
-import { AuthContainer, AuthText, AuthTop } from '@/features/styled/authStyled';
+import { AuthText, AuthTop } from '@/features/styled/authStyled';
 import { Inner } from '@/shared/styled/GlobalStyled';
 import Image from 'next/image';
 import mascot from '@/shared/assets/images/contents/mascot.png';
-import { useSearchParams, useRouter } from 'next/navigation';
-import Button from '@/shared/components/Button';
+import FindIdResult from '@/features/userFind/components/FindIdResult';
 
 const IdFindResultPage = () => {
-    const searchParams = useSearchParams();
-    const userId = searchParams.get('userId');
-    const router = useRouter();
-
     return (
         <>
             <Inner>
@@ -22,42 +17,7 @@ const IdFindResultPage = () => {
                             아이디를 찾았어요!!
                         </AuthText>
                     </AuthTop>
-                    <div 
-                        style={
-                            {
-                                border: '1px solid #ddd',
-                                borderRadius: '5px',
-                                height: '100px',
-                                display: 'flex',
-                                justifyContent: 'center',
-                                alignItems: 'center',
-                                fontSize: '20px',
-                                fontWeight: '700',
-                                marginTop: '20px',
-                            }
-                        }
-                    >
-                        {userId ? userId : '아이디를 찾을 수 없습니다.'}
-                    </div>
-                    <div
-                        style={
-                            {
-                                display: 'flex',
-                                flexDirection: 'column',
-                                gap: '10px',
-                                marginTop: '15px',
-                            }
-                        }
-                    >
-                        <Button 
-                            buttonText='로그인 하러 가기'
-                            onClick={() => router.push('/login')}
-                        />
-                        <Button 
-                            buttonText='비밀번호 찾기'
-                            onClick={() => router.push('/userFind/passwordFind')}
-                        />
-                    </div>    
+                    <FindIdResult />
                 </div>
             </Inner> 
         </>
---
+++ features/userFind/components/FindIdResult.tsx
@@ -0,0 +1,52 @@
+import Button from '@/shared/components/Button';
+import React from 'react';
+import { useSearchParams, useRouter } from 'next/navigation';
+
+const FindIdResult = () => {
+    const searchParams = useSearchParams();
+    const userId = searchParams.get('userId');
+    const router = useRouter();
+
+    return (
+        <div>
+            <div 
+                style={
+                    {
+                        border: '1px solid #ddd',
+                        borderRadius: '5px',
+                        height: '100px',
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        fontSize: '20px',
+                        fontWeight: '700',
+                        marginTop: '20px',
+                    }
+                }
+            >
+                {userId ? userId : '아이디를 찾을 수 없습니다.'}
+            </div>
+            <div
+                style={
+                    {
+                        display: 'flex',
+                        flexDirection: 'column',
+                        gap: '10px',
+                        marginTop: '15px',
+                    }
+                }
+            >
+                <Button 
+                    buttonText='로그인 하러 가기'
+                    onClick={() => router.push('/login')}
+                />
+                <Button 
+                    buttonText='비밀번호 찾기'
+                    onClick={() => router.push('/userFind/passwordFind')}
+                />
+            </div>    
+        </div>
+    );
+};
+
+export default FindIdResult;
