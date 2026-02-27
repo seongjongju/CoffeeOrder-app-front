47a995030ad0f36f751465c50eab2f8ba4e29ca8
seongjongju <sjj990480@gmail.com>
2026-02-27 21:04:17 +0900

    알림창 내림차순 정렬

--- features/hooks/alert/useAlert.tsx
+++ features/hooks/alert/useAlert.tsx
@@ -1,10 +1,18 @@
 'use client';
 import { useAppSelector } from '@/store/hook';
-import React from 'react';
+import React, { useMemo } from 'react';
 
 const useAlert = () => {
     const alert = useAppSelector(state => state.alert);
-    const alertItems = alert.items;
+
+    const alertItems = useMemo(() => {
+        return [...alert.items].sort((a, b) => {
+            const idA = String(a.alertId || '');
+            const idB = String(b.alertId || '');
+            
+            return idB.localeCompare(idA);
+        });
+    }, [alert.items]);
 
     return {alertItems};
 };
